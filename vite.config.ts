7a61fa22b85e8d315186a1c1e47a6f10a50e3c65
Rustam Kurbonov <rmkurbonov@sberbank.ru>
2024-07-08 17:38:33 +0300

    login

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,30 @@ import { defineConfig } from "vite";
 import path, { resolve } from "path";
 import handlebars from "vite-plugin-handlebars";
 import { fileURLToPath } from "url";
+import inspect from "vite-plugin-inspect";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
+  server: {
+    port: 3000,
+    strictPort: true,
+    host: true,
+  },
   root: resolve(__dirname, "src"),
   build: {
     outDir: resolve(__dirname, "dist"),
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        login: resolve(__dirname, "pages/login/index.html"),
+      },
+    },
   },
   plugins: [
+    inspect(),
     handlebars({
-      partialDirectory: resolve(__dirname, "src/partials"),
       context: {
         username: "Name",
       },
