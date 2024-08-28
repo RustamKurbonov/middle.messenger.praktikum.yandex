dc22e9b940c5eca8f6aaee6066b0eb76b6fcb980
Rustam Kurbonov <rmkurbonov@sberbank.ru>
2024-08-28 11:32:45 +0300

    add router test

--- src/serveses/router/Router.test.ts
+++ src/serveses/router/Router.test.ts
@@ -2,11 +2,33 @@ import { Paths } from 'src/share/constants/routes';
 import router from './Router';
 
 describe('Router', () => {
-  it('корректно преобразует объект версии в строку', () => {
-    router.start();
+  router.start();
+  const mockGoBack = jest.fn();
+  const mockGoForward = jest.fn();
 
+  it('правильно запускается', () => {
+    expect(global.window.location.pathname).toBe(Paths.Login);
+  });
+
+  it('правильно работает переход по ссылке', () => {
     router.go(Paths.Chat);
 
-    expect('1.2.3.4').toBe('1.2.3.4');
+    expect(global.window.location.pathname).toBe(Paths.Chat);
+  });
+
+  it('правильно работает переход назад', () => {
+    router.back({ back: mockGoBack } as unknown as History);
+
+    expect(mockGoBack).toHaveBeenCalled();
+  });
+
+  it('правильно работает переход вперед', () => {
+    router.forward({ forward: mockGoForward } as unknown as History);
+
+    expect(mockGoForward).toHaveBeenCalled();
+  });
+
+  it('правильно находит нужный путь', () => {
+    expect(router.getRoute(Paths.Profile)).not.toBeNull();
   });
 });
